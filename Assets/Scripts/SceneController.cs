b7d9fb6f4f23baea19ed52ef671c43a737991124
CIngram82 <cingram82@gmail.com>
2019-02-22 15:32:07 -0500

    Menus Update
    Updated Menu system fixed broken button on credits page. All menu buttons work (Quit not tested)

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour {
+    int activeSceneBuildInt;
+    [SerializeField] float timeToWait = 2.5f;
+    // Use this for initialization
+    void Start () {
+         activeSceneBuildInt = SceneManager.GetActiveScene().buildIndex;
+        if (activeSceneBuildInt == 0)
+        {
+            StartCoroutine(WaitForTime());
+        }
+    }
+	
+    IEnumerator WaitForTime()
+    {
+        yield return new WaitForSeconds(timeToWait);
+        LoadNextScene();
+    }
+
+	
+
+
+    public void ReloadScene()
+    {
+        int activeSceneBuildInt = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(activeSceneBuildInt);
+    }
+    public void LoadNextScene()
+    {
+        int activeSceneBuildInt = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(activeSceneBuildInt + 1);
+    }
+
+    public void LoadSceneByName(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
