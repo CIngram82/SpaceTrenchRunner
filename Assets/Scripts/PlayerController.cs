270852503b1fff90424baac90866dfa44d883036
christopher ingram <cingram82@gmail.com>
2019-02-22 13:46:14 -0500

    Adding Files

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float fowardSpeed = 100.0f;
+    public float speed = 100.0f;
+    private Rigidbody  playerRB;
+    private float horizontalMove;
+    private float verticalMove;
+    public ParticleSystem wallhit;
+	// Use this for initialization
+	void Start () {
+        playerRB = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        horizontalMove = Input.GetAxis("Horizontal");
+        verticalMove = Input.GetAxis("Vertical");
+	}
+    private void FixedUpdate()
+    {
+        playerRB.velocity = transform.forward * fowardSpeed;
+
+        Vector3 movement = transform.forward;
+        movement.x = horizontalMove * speed;
+        movement.y = verticalMove * speed;
+        playerRB.AddForce( movement);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("wall"))
+        {
+            Vector3 collisionPoint = collision.contacts[0].point;
+            Vector3 dir = collisionPoint - transform.position;
+            dir = -dir.normalized;
+            playerRB.AddForce(dir * (speed * 5));
+            Instantiate(wallhit, collisionPoint, Quaternion.Euler(dir),collision.gameObject.transform);
+        }else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            Vector3 collisionPoint = collision.contacts[0].point;
+            Vector3 dir = collisionPoint - transform.position;
+            dir = -dir.normalized;
+            playerRB.AddForce(dir * (speed * 10));
+            Instantiate(wallhit, collisionPoint, Quaternion.Euler(dir), collision.gameObject.transform);
+            Instantiate(wallhit, collisionPoint, Quaternion.Euler(dir), collision.gameObject.transform);
+            Instantiate(wallhit, collisionPoint, Quaternion.Euler(dir), collision.gameObject.transform);
+        }
+
+    }
+    /*
+     * // force is how forcefully we will push the player away from the enemy.
+ float force = 3;
+
+ // If the object we hit is the enemy
+ if (c.gameObject.tag == "enemy")
+ {
+     // Calculate Angle Between the collision point and the player
+     Vector3 dir = c.contacts[0].point - transform.position;
+     // We then get the opposite (-Vector3) and normalize it
+     dir = -dir.normalized;
+     // And finally we add force in the direction of dir and multiply it by force. 
+     // This will push back the player
+     GetComponent<Rigidbody>().AddForce(dir*force);
+ }
+     */
+
+}
---
+++ Assets/Scripts/SpawnObstacle.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObstacle : MonoBehaviour {
+    private Vector3 startPoint;
+    private Vector3 endPoint;
+    private Transform wall1;
+    private Transform wall2;
+    public GameObject CubeObstacle;
+    // Use this for initialization
+    void Start () {
+
+        wall1 = gameObject.transform.Find("Wall1").transform;
+        startPoint = GetVector3ForWall(wall1);
+
+        wall2 = gameObject.transform.Find("Wall2").transform;
+        endPoint = GetVector3ForWall(wall2);
+
+        SpawnCube();
+    }
+    Vector3 GetVector3ForWall(Transform wall)
+    {
+        
+        Vector3 wallPos = wall.position;
+        Vector3 wallScale = wall.localScale;
+        float wallx = wallPos.x;
+        float wally = Random.Range((float)(wallPos.y - (wallScale.y * .5)), (float)(wallPos.y + (wallScale.y * .5)));
+        float wallz = Random.Range((float)(wallPos.z - (wallScale.z * .5)), (float)(wallPos.z + (wallScale.z * .5)));
+        return new Vector3(wallx, wally, wallz);
+    }
+ 
+	
+    void SpawnCube()
+    {
+        // spawn a cube obstacle prefab and move it the center point between startpoint and end point. alighing it with the direction between the 2
+        // and adjusting its size to fill the space between the 2.
+        float dist = Vector3.Distance(startPoint, endPoint);
+        Vector3 size = new Vector3(0.5f, 0.5f, dist);
+        GameObject cubeOb1 = Instantiate(CubeObstacle, transform);
+        
+        cubeOb1.transform.position = startPoint;
+        cubeOb1.transform.localScale = size;
+        cubeOb1.transform.LookAt(endPoint);
+        cubeOb1.transform.Rotate(0,0,45);
+        cubeOb1.transform.position = Vector3.MoveTowards(startPoint, endPoint, (float)(dist * 0.5));
+        
+    }
+
+}
