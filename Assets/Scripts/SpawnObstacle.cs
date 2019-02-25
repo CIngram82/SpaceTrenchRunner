acc842f4a6d25fd7b31beae3f2e8d6ddd0a55e15
christopher ingram <cingram82@gmail.com>
2019-02-25 15:26:14 -0500

    Cube destruction Started
    Cubes are now a list of objects. Added script to each cube to track destroying it

--- Assets/Scripts/SpawnObstacle.cs
+++ Assets/Scripts/SpawnObstacle.cs
@@ -10,6 +10,7 @@ public class SpawnObstacle : MonoBehaviour {
     private Transform ground;
     private Transform roof;
     public GameObject CubeObstacle;
+    public List<GameObject> CubeList = new List<GameObject>();
     // Use this for initialization
     void Start () {
 
@@ -17,17 +18,12 @@ public class SpawnObstacle : MonoBehaviour {
         startPoint = GetVector3ForTransform(wall1);
 
         wall2 = gameObject.transform.Find("Wall2").transform;
-        endPoint = GetVector3ForTransform(wall2);
+        endPoint = startPoint;
+        endPoint.x = wall2.transform.position.x;
 
         SpawnCube();
 
-        ground = gameObject.transform.Find("Ground").transform;
-        startPoint = GetVector3ForTransform(ground);
-
-        roof = gameObject.transform.Find("Roof").transform;
-        endPoint = GetVector3ForTransform(roof);
-
-        SpawnCube();
+        
     }
 
 
@@ -36,9 +32,9 @@ public class SpawnObstacle : MonoBehaviour {
         
         Vector3 wallPos = wall.position;
         Vector3 wallScale = wall.localScale;
-        float wallx = Random.Range((float)(wallPos.x - (wallScale.x * .5)), (float)(wallPos.x + (wallScale.x * .5)));
-        float wally = Random.Range((float)(wallPos.y - (wallScale.y * .5)), (float)(wallPos.y + (wallScale.y * .5)));
-        float wallz = Random.Range((float)(wallPos.z - (wallScale.z * .5)), (float)(wallPos.z + (wallScale.z * .5)));
+        float wallx =  wallPos.x + (float)(wallScale.x * 0.5);
+        float wally = Random.Range((float)(wallPos.y - (wallScale.y * .5))+1, (float)(wallPos.y + (wallScale.y * .5))-1);
+        float wallz = Random.Range((float)(wallPos.z - (wallScale.z * .5))+1, (float)(wallPos.z + (wallScale.z * .5))-1);
         return new Vector3(wallx, wally, wallz);
     }
  
@@ -47,16 +43,21 @@ public class SpawnObstacle : MonoBehaviour {
     {
         // spawn a cube obstacle prefab and move it the center point between startpoint and end point. alighing it with the direction between the 2
         // and adjusting its size to fill the space between the 2.
+
+        
         float dist = Vector3.Distance(startPoint, endPoint);
-        Vector3 size = new Vector3(0.5f, 0.5f, dist);
-        GameObject cubeOb1 = Instantiate(CubeObstacle, transform);
-        
-        cubeOb1.transform.position = startPoint;
-        cubeOb1.transform.localScale = size;
-        cubeOb1.transform.LookAt(endPoint);
-        cubeOb1.transform.Rotate(0,0,45);
-        cubeOb1.transform.position = Vector3.MoveTowards(startPoint, endPoint, (float)(dist * 0.5));
-        
+        Vector3 size = new Vector3(0.5f, 0.5f, 1.0f);
+        for (int i = 0; i < dist; i++)
+        {
+            GameObject cubeOb1 = Instantiate(CubeObstacle, transform);
+
+            cubeOb1.transform.position = startPoint;
+            cubeOb1.transform.localScale = size;
+            cubeOb1.transform.LookAt(endPoint);
+            cubeOb1.transform.Rotate(0, 0, 45);
+            cubeOb1.transform.position = Vector3.MoveTowards(startPoint, endPoint, i);
+            CubeList.Add(cubeOb1);
+        }
     }
 
 }
