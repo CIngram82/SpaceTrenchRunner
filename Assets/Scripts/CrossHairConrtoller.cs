0d123840cffbb71a636df4661c208115e1ca1fd1
CIngram82 <cingram82@gmail.com>
2019-02-26 21:43:39 -0500

    Start of Laser ray cast
    Setting up the Groundwork for the laser system

---
+++ Assets/Scripts/CrossHairConrtoller.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CrossHairConrtoller : MonoBehaviour {
+
+    // Use this for initialization
+    void Start ()
+    {
+        Cursor.visible = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        UpdateCrossHairPos();
+        if (Input.GetMouseButtonDown(0))
+        {
+            FireLaser();
+        }
+
+    }
+
+    void UpdateCrossHairPos()
+    {
+        float mouseX = Input.mousePosition.x;
+        float mouseY = Input.mousePosition.y;
+
+        Vector2 newPos = new Vector2(mouseX, mouseY);
+        gameObject.transform.position = newPos;
+    }
+
+    void FireLaser()
+    {
+        print("Pew! ");
+
+    }
+
+}
